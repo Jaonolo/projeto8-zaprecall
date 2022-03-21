1f70cf9aebed5d4f5939ea3bbdcd227aa23a5ce0
João Vitor Sanches <joaovitormanolo@gmail.com>
2022-03-21 09:00:12 -0300

    feat : adding icon list at home footer (note: bad implementation)

--- src/home/home-footer/HomeFooter.jsx
+++ src/home/home-footer/HomeFooter.jsx
@@ -1,10 +1,29 @@
+import { useState } from "react"
+
 const HomeFooter = ({current, total}) => {
-    let currentCount = current 
-        .filter(e => ['fail', 'almost', 'success'].includes(e))
-        .length
+    const [doneDisplay, setDisplay] = useState([])
+    const [lastList, setLast] = useState(new Array(total).fill(''))
+
+    const icons = {
+        fail: "close-circle",
+        almost: "help-circle",
+        success: "checkmark-circle",
+    }
+
+    let currentCount = current.filter(
+        e => ['fail', 'almost', 'success'].includes(e)
+    )
+   
+    if(currentCount.length !== doneDisplay.length){
+        setDisplay([...doneDisplay, current.filter((e, i) => e !== lastList[i])])
+        setLast(current)
+    }
 
     return <footer className="home-footer">
-        {currentCount}/{total} CONCLUÍDOS
+        {currentCount.length}/{total} CONCLUÍDOS
+        <div>
+            {doneDisplay.map(e => <ion-icon name={icons[e]}/>)}
+        </div>
     </footer>   
 }
 

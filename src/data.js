a6837537e603ee1c912f9bf700c9d3ffd29f359b
Briofita09 <felipe.osorio.santos@hotmail.com>
2022-03-24 16:49:08 -0300

    Preparando revisão de código

--- src/data.js
+++
@@ -1,6 +0,0 @@
-const data = {
-    test: [{question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}, {question: 'a', answer: 'b'}],
-    test2: [{question: 'ximblas', answer: 'sdksdksod'}]
-}
-
-export default data
--- src/welcome/start-button/StartButton.jsx
+++
@@ -1,8 +0,0 @@
-const StartButton = ({startApp}) => {
-
-    return <button className="start-button" onClick={startApp}>
-        Iniciar Recall!
-    </button>
-}
-
-export default StartButton
